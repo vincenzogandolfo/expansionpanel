91f4906f5246941d694fd4fcc16ecf2236427b13
vg_dev <vincenzo.gandolfo7@gmail.com>
2023-01-12 19:29:31 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/my/vgdev/expansionpanel/MainActivity.kt
@@ -0,0 +1,6 @@
+package my.vgdev.expansionpanel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Center(child: Text('ExpansionPanel'))),
+        body: MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+// stores ExpansionPanel state information
+class Item {
+  String expandedValue;
+  String headerValue;
+  bool isExpanded;
+
+  Item({
+    required this.expandedValue,
+    required this.headerValue,
+    this.isExpanded = false,
+  });
+}
+
+List<Item> generateItems(int numberOfItems) {
+  return List<Item>.generate(numberOfItems, (int index) {
+    return Item(
+      headerValue: 'Pannello ${index + 1}',
+      expandedValue: 'Questo è il Pannello numero ${index + 1}',
+    );
+  });
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  final List<Item> _data = generateItems(5);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        child: _buildPanel(),
+      ),
+    );
+  }
+
+  Widget _buildPanel() {
+    return ExpansionPanelList(
+      expansionCallback: (int index, bool isExpanded) {
+        setState(() {
+          _data[index].isExpanded = !isExpanded;
+        });
+      },
+      children: _data.map<ExpansionPanel>((Item item) {
+        return ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return ListTile(
+              title: Text(item.headerValue),
+            );
+          },
+          body: ListTile(
+              title: Text(item.expandedValue),
+              subtitle:
+                  const Text('Per Eliminare il Pannello, premere il Cestino!'),
+              trailing: const Icon(Icons.delete),
+              onTap: () {
+                setState(() {
+                  _data.removeWhere((Item currentItem) => item == currentItem);
+                });
+              }),
+          isExpanded: item.isExpanded,
+        );
+      }).toList(),
+    );
+  }
+}
